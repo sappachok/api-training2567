5720d2aa034c9a947a30c70df76537b70a8da6ca
sappachok <suppachok_sin@nstru.ac.th>
2024-11-04 11:27:13 +0700

    update

--- one.php
+++ one.php
@@ -12,4 +12,15 @@
     echo "<pre>";
     var_dump($decode);
     echo "</pre>";
+    echo "<p>".$decode->car."<p>";
+   
+    $data = array(
+        array("name"=>"batman", "age"=>30, "car"=>"Batmobile"),
+        array("name"=>"superman", "age"=>40, "car"=>"Supercar")
+    );
+
+    echo "<b>Array Data</b>";
+    echo "<pre>";
+    var_dump($data);
+    echo "</pre>";
 ?>
