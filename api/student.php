065af2020147333be81b7e3014fafc5e30a2473d
sappachok <suppachok_sin@nstru.ac.th>
2024-11-04 14:52:53 +0700

    update

---
+++ api/student.php
@@ -0,0 +1,23 @@
+[
+   {
+      "code":"4411425001",
+      "name":"Johny Dev",
+      "email":"4411425001@nstru.ac.th",
+      "tel":"0812345670",
+      "picture":"4411425001.jpg"
+   },
+   {
+      "code":"4411425002",      
+      "name":"Sarah Connor",
+      "email":"4411425002@nstru.ac.th",
+      "tel":"0812345555",
+      "picture":"4411425002.jpg"
+   },
+   {
+      "code":"4411425003",      
+      "name":"Bruce Wayne",
+      "email":"4411425003@nstru.ac.th",
+      "tel":"0812347777",
+      "picture":"4411425003.jpg"
+   }
+]
